5aa9f65305178fa5c2d8b8c09958a52eb7061aba
Ellis <ellchung@cs.stonybrook.edu>
2021-05-28 23:08:10 -0400

    Added 3 empty pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Results from './pages/Results';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Router>
+    {/* Navigation bar with home redirect and search */} 
+    <Navbar />
+    {/* Main content of website with routes */}
+    <main>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/search" exact>
+          <Search />
+        </Route>
+        <Route path="/results" exact>
+          <Results />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </main>
+  </Router>;
 }
 
 export default App;

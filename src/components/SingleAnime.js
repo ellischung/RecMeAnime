99b27f2c911af68fa78d51f9fb1b731087a662d7
Ellis <ellischung98@gmail.com>
2021-06-28 03:19:59 -0400

    added down arrows after single anime

--- src/components/SingleAnime.js
+++ src/components/SingleAnime.js
@@ -7,43 +7,68 @@ const SingleAnime = (props) => {
     // information to be displayed for each anime
     const {title, image_url, score, episodes, premiered, rating, synopsis} = props.info;
 
+    // for down arrow mapping
+    const arrows = ['first', 'second', 'third'];
+
     return (
-        // display single anime + at most 10 recommendations
-        <Grid
-            container
-            spacing={10}
-            direction="row" 
-            justify="center" 
-            alignItems="center" 
-            alignContent="center"
-            className="singleAnime__container"
-        >
-            <Grid item>
-                <img src={image_url} alt={title} className="singleAnime__image" />
+        <React.Fragment>
+            {/* display single anime + at most 10 recommendations */}
+            <Grid
+                container
+                spacing={10}
+                direction="row" 
+                justify="center" 
+                alignItems="center" 
+                alignContent="center"
+                className="singleAnime__container"
+            >
+                <Grid item>
+                    <img src={image_url} alt={title} className="singleAnime__image" />
+                </Grid>
+                <Grid item>
+                    <Paper elevation={3} className="singleAnime__description">
+                        <Typography variant="h4" component="h2">
+                            {title}
+                        </Typography>
+                        <Typography variant="h5" component="h2" style={{display: "flex", alignItems: "center", flexWrap: "wrap"}}>
+                            <StarBorderIcon fontSize="medium" color="primary" />{score}
+                        </Typography>
+                        <Typography variant="h5" component="h2">
+                            Episodes: {episodes}
+                        </Typography>
+                        <Typography variant="h5" component="h2">
+                            Premiered: {premiered}
+                        </Typography>
+                        <Typography variant="h5" component="h2" paragraph={true}>
+                            Rating: {rating}
+                        </Typography>
+                        <Typography variant="h5" component="h2" style={{width: 1000}}>
+                            {synopsis}
+                        </Typography>
+                    </Paper>
+                </Grid>
             </Grid>
-            <Grid item>
-                <Paper elevation={3} className="singleAnime__description">
-                    <Typography variant="h4" component="h2">
-                        {title}
-                    </Typography>
-                    <Typography variant="h5" component="h2" style={{display: "flex", alignItems: "center", flexWrap: "wrap"}}>
-                        <StarBorderIcon fontSize="medium" color="primary" />{score}
-                    </Typography>
-                    <Typography variant="h5" component="h2">
-                        Episodes: {episodes}
-                    </Typography>
-                    <Typography variant="h5" component="h2">
-                        Premiered: {premiered}
-                    </Typography>
-                    <Typography variant="h5" component="h2" paragraph={true}>
-                        Rating: {rating}
-                    </Typography>
-                    <Typography variant="h5" component="h2" style={{width: 1000}}>
-                        {synopsis}
-                    </Typography>
-                </Paper>
+            {/* 3 animated arrows pointing to recommendations */}
+            <Grid
+                container
+                direction="row" 
+                justify="center" 
+                alignItems="center" 
+                alignContent="center"
+                className="arrow__container"
+            >
+                {arrows.map((arrow) => {
+                    return <Grid item className="arrow__down" style={{margin: "50px"}}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </Grid>
+                })}
             </Grid>
-        </Grid>
+            {/* list of at most 10 recommendations */}
+        </React.Fragment>
     );
 };
 

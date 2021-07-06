35f13ca2e94afb0e904eb1770e247f180dc8ec0f
Ellis <ellischung98@gmail.com>
2021-07-06 19:22:59 -0400

    added modal for score statistics

--- src/components/SingleAnime.js
+++ src/components/SingleAnime.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import { Grid, Typography, Paper, Tooltip, Button} from "@material-ui/core";
+import React, { useState } from 'react';
+import { Grid, Typography, Paper, Button, Modal} from "@material-ui/core";
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import './SingleAnime.scss'
 
 const SingleAnime = (props) => {
     // information to be displayed for each anime
     const {title, image_url, score, episodes, premiered, rating, synopsis} = props.info;
-    const doStuff = () => {
-        Object.keys(props.scores).forEach((score) => {
-            console.log(props.scores[score].votes, props.scores[score].percentage)
-        })
+
+    // state hook for modal
+    const [open, setOpen] = useState(false);
+    
+    // open modal handler function
+    const handleOpen = () => {
+        setOpen(true);
     };
 
+    // close modal handler function
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    // score stats to be displayed by modal
+    const scores = (
+        <div style={{width: "600px", backgroundColor: "lightgray", border: "2px solid #000000"}}>
+            {Object.keys(props.scores).map((score) => (
+                <div>
+                    {score} {props.scores[score].percentage} {props.scores[score].votes}
+                </div>
+            ))}
+        </div>
+    );
+
     return (
-        // display the single anime with all relevant information
+        // display the single anime with all of the information from props
         <Grid
             container
             spacing={10}
@@ -34,13 +53,27 @@ const SingleAnime = (props) => {
                     <Typography variant="h5" component="h2" style={{display: "flex", alignItems: "center", flexWrap: "wrap"}}>
                         <StarBorderIcon fontSize="medium" color="primary" />
                         {score}
-                        <Tooltip title={doStuff} placement="top" arrow>
-                            <Button onClick={doStuff} variant="outlined" color="primary" size="small" style={{marginLeft: "20px"}}>
+                        {/* Modal for stats */}
+                        <div>
+                            <Button onClick={handleOpen} variant="outlined" color="primary" size="small" style={{marginLeft: "20px"}}>
                                 <Typography variant="body2" component="h2">
                                     Stats
                                 </Typography>
                             </Button>
-                        </Tooltip>
+                            <Modal
+                                open={open}
+                                onClose={handleClose}
+                                style={{
+                                    display: "flex", 
+                                    alignItems: "center", 
+                                    flexWrap: "wrap", 
+                                    left: "30%"
+                                }}
+                            >
+                                {scores}
+                            </Modal>
+                        </div>
+                        {/* End of modal for stats */}
                     </Typography>
                     <Typography variant="h5" component="h2">
                         Episodes: {episodes}
